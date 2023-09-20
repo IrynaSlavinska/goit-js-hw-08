69bcb5dc01764d413e0d3dde1440c4876a9fa6d9
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2023-09-20 09:50:28 +0300

    markup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,24 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+console.log(galleryEl);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup(galleryItems));
+
+function galleryMarkup(array) {
+  return array
+    .map(
+      ({ preview, original, description }) => `
+  <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+   </a>
+</li>
+    `
+    )
+    .join('');
+}
