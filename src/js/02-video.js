f0ead271bccc130ba650e4efc4f4b8938c5fb1cf
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2023-09-21 12:48:50 +0300

    second

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const currentTime = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(videoUpdate, 1000));
+
+function videoUpdate({ seconds }) {
+  localStorage.setItem(currentTime, seconds);
+}
+
+const currentMoment = localStorage.getItem(currentTime);
+console.log(currentMoment);
+
+player.setCurrentTime(currentMoment);
