369b2d3ebc0a994238d306f30e9c44f1d76467a6
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2023-09-22 10:15:26 +0300

    task-3 commit-1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,10 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const feedbackInput = document.querySelector('.feedback-form input');
+const feedbackMessage = document.querySelector('.feedback-form textarea');
+
+const storageKey = 'feedback-form-state';
+
+feedbackForm.addEventListener('submit', onSubmitClick);
+feedbackForm.addEventListener('input', throttle(onInputClick, 500));
