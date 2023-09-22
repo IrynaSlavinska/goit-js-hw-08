f1e04b7deffa0ad28d95abdf899ed09b9e09504c
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2023-09-22 17:13:41 +0300

    third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,61 @@
 import throttle from 'lodash.throttle';
 
-const feedbackForm = document.querySelector('.feedback-form');
-const feedbackInput = document.querySelector('.feedback-form input');
-const feedbackMessage = document.querySelector('.feedback-form textarea');
+const refs = {
+  feedbackForm: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
 
-const storageKey = 'feedback-form-state';
+const STORAGE_KEY = 'feedback-form-state';
 
-feedbackForm.addEventListener('submit', onSubmitClick);
-feedbackForm.addEventListener('input', throttle(onInputClick, 500));
+refs.feedbackForm.addEventListener('submit', onSubmitClick);
+refs.feedbackForm.addEventListener('input', throttle(onInput, 500));
+
+let inputValue = {};
+getTextArea();
+
+function onSubmitClick(event) {
+  event.preventDefault();
+
+  savedMessage();
+
+  const formElements = event.currentTarget.elements;
+  // console.log(formElements);
+  const email = formElements.email.value;
+  const message = formElements.message.value;
+
+  if (!email || !message) {
+    alert('Заповніть всі поля');
+  } else {
+    localStorage.removeItem(STORAGE_KEY);
+    refs.feedbackForm.reset();
+    // console.log(inputValue);
+    inputValue = { email: '', message: '' };
+  }
+}
+
+function onInput(event) {
+  savedMessage();
+  inputValue[event.target.name] = event.target.value;
+  // console.log(event.target.value);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(inputValue));
+}
+
+function getTextArea() {
+  if (localStorage.getItem(STORAGE_KEY)) {
+    inputValue = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    for (const key in inputValue) {
+      if (inputValue[key]) {
+        console.log(refs.feedbackForm.elements);
+        refs.feedbackForm.elements[key].value = inputValue[key];
+      }
+    }
+  }
+}
+
+function savedMessage() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedMessage) {
+    inputValue = savedMessage;
+  }
+}
